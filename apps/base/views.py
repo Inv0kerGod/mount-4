64283e1d6097af5e96a4d4afc8c07faa459c03fd
Inv0kerGod <122999445+Inv0kerGod@users.noreply.github.com>
2024-06-29 13:16:54 +0600

    hw3

---
+++ apps/base/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from apps.base.models import Base
+# Create your views here.
+
+def index(request):
+    base = Base.objects.latest('id')
+    return render(request, 'index-dark.html',locals())
+
+def about_us(request):
+    return render(request, 'about-us-dark.html',locals())
+
+
